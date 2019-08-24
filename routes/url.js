0ebde6d7c23f00a3f0ebc87d9a4673c1ed088a3e
leduyhiep <leduyhiep2612@gmail.com>
2019-08-24 19:35:06 +0700

    set env to deploy on heroku

--- routes/url.js
+++ routes/url.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const validUrl = require('valid-url');
 const shortid = require('shortid');
-const config = require('config');
 
 const Url = require('../models/Url');
 
 // @route     POST /api/url/shorten
 // @desc      Create short URL
 router.post('/shorten', async (req, res) => {
-  const baseUrl = config.get('baseUrl');
   const { longUrl, shortUrl } = req.body;
+  const baseUrl = process.env.BASE_URL;
 
   // Check base url
   if (!validUrl.isUri(baseUrl)) {
